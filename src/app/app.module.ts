5947c8d607b5e36bad5000be3e2d39a1d05af852
Sadegh <narmafraz@gmail.com>
2020-04-10 01:13:11 +0100

    Add NGXS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxsStoreModule } from '@store/store.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsStoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]

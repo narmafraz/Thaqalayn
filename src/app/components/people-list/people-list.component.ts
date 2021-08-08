95d7d5859fd194f5d0140114a7d7897198f9e01c
narmafraz <narmafraz@gmail.com>
2021-08-09 00:10:00 +0100

    Fix sort on narrator index

--- src/app/components/people-list/people-list.component.ts
+++ src/app/components/people-list/people-list.component.ts
@@ -1,3 +1,4 @@
+import { KeyValue } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { BooksState } from '@store/books/books.state';
@@ -13,4 +14,13 @@ export class PeopleListComponent {
 
   @Select(BooksState.getNarratorIndex) narrators$: Observable<Record<number, string>>;
 
+  keyAscOrder = (a: KeyValue<number,string>, b: KeyValue<number,string>): number => {
+    const an = +a.key
+    const bn = +b.key
+    return an > bn ? 1 : (bn > an ? -1 : 0);
+  }
+
+  nameOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
+    return a.value.localeCompare(b.value);
+  }
 }
